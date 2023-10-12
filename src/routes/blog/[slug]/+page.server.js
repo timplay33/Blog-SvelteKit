e5c2a847d14d470838d7884847600cd5fbbb926b
Tim Heidler <tim@heidler.eu.org>
2023-10-12 22:25:13 +0200

    fixed 500 error on wrong slug to 404 not found

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -5,7 +5,10 @@ export async function load({params}) {
 
     
 let { data } = await supabase.from('posts').select().eq('slug', params.slug);
+  if (data[0] == undefined) {
+    throw error(404, "Not Found");
 
+  }
     return {
       articles: data ?? [],
     };
