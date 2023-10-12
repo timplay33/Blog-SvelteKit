4ba0b0f13a79531bab1b67a30413d5e41ef3fbc2
Tim Heidler <tim@heidler.eu.org>
2023-10-12 22:34:17 +0200

    added error page (404 & 500)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<div>
+			<h1 class="text-5xl font-bold">{$page.status} {$page.error.message}</h1>
+			{#if $page.status == 404}
+				<p class="py-6">The page you were looking for could not be found.</p>
+			{:else if $page.status == 500}
+				<p class="py-6">The server encountered an internal error.</p>
+			{/if}
+			<a href="javascript:history.back()" class="btn btn-secondary">Get back</a>
+			<span>or go to</span>
+			<a href="/" class="btn btn-secondary">Home</a>
+		</div>
+	</div>
+</div>
