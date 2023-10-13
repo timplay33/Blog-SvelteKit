44772704467272b710a1110ea515a25a8e0d068d
Tim Heidler <tim@heidler.eu.org>
2023-10-13 16:46:52 +0200

    modified:   src/routes/+layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,6 @@
 	import Footer from './Footer.svelte';
 </script>
 
-<svelte:head>
-	<title>Tim Heidler</title>
-	<meta
-		name="description"
-		content="This is my blog! I use this space to document my adventures in development, sharing how I’ve overcome issues
-	I’ve run into, useful resources, and more."
-	/>
-</svelte:head>
-
 <Header />
 <div class="min-h-screen">
 	<slot />
