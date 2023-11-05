aabfc2ec6f15bdba564c95dcaba15c6dffe67dac
Tim <tim3play3@gmail.com>
2023-11-05 23:25:30 +0100

    Fix #4 wrong orientation of posts

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-  const { data } = await supabase.from("posts").select();
+  const { data } = await supabase.from("posts").select().order('created_at', { ascending: false });
   return {
     posts: data ?? [],
   };
